003b8a9d17e88446713d39d46fdd70ebfe844767
Cyrus WaChong <31861937+cyruswachong@users.noreply.github.com>
2019-04-11 13:25:10 -0700

    Fixed Misnamed Files

--- 5) Mask-Based image processing/Questions/Functions/Composite_mask.m
+++ 5) Mask-Based image processing/Questions/Functions/Composite_mask.m
@@ -1,4 +1,16 @@
-function Noise_Image = Add_noise(Image, Mean, Variance)
-Noisy_image = imnoise(Grayscale,'gaussian', Mean, Variance);
+function [final_val, enhanced_composite] = Composite_mask(Grayscaled, padded_image, Scalar_Factor)
+
+[rows, cols] = size(Grayscaled); 
+final_val = zeros(size(Grayscaled));
+
+for i = 2:(rows-1)
+    for j = 2:(cols-1)
+        final_val(i,j) = Scalar_Factor*(padded_image(i-1,j) - 8*padded_image(i,j) + padded_image(i,j+1)...
+            + padded_image(i+1,j) + padded_image(i,j-1) + padded_image(i+1,j+1) +...
+            padded_image(i-1, j+1) + padded_image(i-1,j-1) + padded_image(i+1,j-1));
+    end
 end
 
+enhanced_composite = Grayscaled - final_val;
+rescale(enhanced_composite);
+end
---
+++ 5) Mask-Based image processing/Questions/Functions/Sobel_mask.m
@@ -0,0 +1,16 @@
+function [final_val, enhanced_sobel] = Sobel_mask(Grayscaled, padded_image,Scalar_Factor)
+
+[rows, cols] = size(Grayscaled);  
+final_val = zeros(size(Grayscaled));
+
+%% Sobel Mask 
+for i = 2:(rows-1)
+    for j = 2:(cols-1)
+        final_val(i,j) = Scalar_Factor*(padded_image(i-1,j) - 2*padded_image(i,j) + padded_image(i,j+1));
+    end
+end
+
+enhanced_sobel = Grayscaled - final_val;
+rescale(enhanced_sobel);
+
+end
